db0084b6ae44ba5d1d1d4f43fa92c7ace3e85022
Frances Thai <notfelineit@gmail.com>
2020-04-08 18:16:07 -0700

    Add initial first form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import Form from './components/Form'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Meowdy, world!
-      </header>
+      <Form />
     </div>
   );
 }
--- src/forms/types.ts
+++ src/forms/types.ts
@@ -1,4 +1,5 @@
 export interface Form {
+  state: string
   pages: Page[]
 }
 
