4efcd732e2db6245d527162c4a30102a8df4d9e0
Alex Gaesser <agaesser@gmail.com>
2020-04-11 13:19:32 -0700

    init auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
 import './App.css';
 import FormApp from './components/FormApp'
+import { withAuthenticator } from 'aws-amplify-react';
+import '@aws-amplify/ui/dist/style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <FormApp />
-    </div>
-  );
+Amplify.configure(awsconfig);
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <FormApp/>
+            </div>
+        );
+    }
+
 }
-
-export default App;
+export default withAuthenticator(App, true);
