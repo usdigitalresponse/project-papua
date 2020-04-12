41659dea12306b297c2d2a38180f892bfabaa09c
Colin <me@colinking.co>
2020-04-12 12:51:03 -0700

    Add dependency for Lambda on S3

--- backend/functions/transformer/src/index.ts
+++ backend/functions/transformer/src/index.ts
@@ -8,14 +8,14 @@ Amplify Params - DO NOT EDIT */
 export async function handler() {
   // TODO implement
 
-  const version = "v0.0.2";
+  const version = "v0.0.3";
 
   const response = {
     statusCode: 200,
     body: JSON.stringify(`Hello from Lambda! (version=${version})`),
   };
 
-  console.log(`responding to a lambda invocation! (version=${version})`);
+  console.log(`responding to a lambda invocation! (version=${version}; raw-bucket=${process.env.RAW_S3_BUCKET_NAME})`);
 
   return response;
 }
