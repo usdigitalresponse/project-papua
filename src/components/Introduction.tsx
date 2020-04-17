8b959c7430771b08b9c4216d7a181a99d941c08c
Colin "👋" King <me@colinking.co>
2020-04-17 09:27:40 -0700

    Update introduction page content

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -14,39 +14,67 @@ const Introduction: React.FC = () => {
           color="black"
           margin={{ top: "none", bottom: "none" }}
         >
-          {translate(getCopy("welcome"), language)}
+          {translate(getCopy("introduction:header"), language)}
         </Heading>
         <Paragraph fill={true} color="black">
-          {translate(getCopy("intro-1"), language)}
+          {translate(getCopy("introduction:welcome"), language)}
           <br />
           <br />
-          {translate(getCopy("intro-2"), language)}
+          {translate(getCopy("introduction:background"), language)}
           <br />
           <br />
-          {getCopy("intro-3") &&
-            <>
-              {translate(getCopy("intro-3"), language)}
-              <br />
-              <br />
-            </>
-          }
-          <Text weight={600}>{translate(getCopy("prereqs"), language)}</Text>
+          {translate(getCopy("introduction:payment"), language)}
+          <br />
+          <br />
+          <Text weight={600}>{translate(getCopy("preparation:header"), language)}</Text>
+          <br />
+          <Text weight={400}>{translate(getCopy("preparation:instructions"), language)}</Text>
+          <ul color="black">
+            <li color="black">{translate(getCopy("preparation:ssn"), language)}</li>
+            <li color="black">{translate(getCopy("preparation:agi"), language)}</li>
+            <li color="black">
+              {translate(getCopy("preparation:employment"), language)}
+            </li>
+            <li color="black">{translate(getCopy("preparation:arn"), language)}</li>
+            <li color="black">
+              {translate(getCopy("preparation:children"), language)}
+            </li>
+            <li color="black">
+              {translate(getCopy("preparation:license"), language)}
+            </li>
+            <li color="black">
+              {translate(getCopy("preparation:bank"), language)}
+            </li>
+          </ul>
+          <br />
+          <Text weight={600}>{translate(getCopy("eligibility:header"), language)}</Text>
+          <br />
+          <Text weight={400}>{translate(getCopy("eligibility:instructions"), language)}</Text>
+          <ul color="black">
+            <li color="black">{translate(getCopy("eligibility:workers"), language)}</li>
+            <li color="black">{translate(getCopy("eligibility:unemployed"), language)}</li>
+            <li color="black">
+              {translate(getCopy("eligibility:no-fault"), language)}
+            </li>
+          </ul>
+          <br />
+          <Text weight={400}>{translate(getCopy("eligibility:other-state"), language)}</Text>
+          <br />
+          <br />
+          <Text weight={400}>{translate(getCopy("pandemic-eligibility:instructions"), language)}</Text>
           <ol color="black">
-            <li color="black">{translate(getCopy("prereqs:ssn"), language)}</li>
-            <li color="black">{translate(getCopy("prereqs:agi"), language)}</li>
-            <li color="black">
-              {translate(getCopy("prereqs:employment"), language)}
-            </li>
-            <li color="black">{translate(getCopy("prereqs:arn"), language)}</li>
-            <li color="black">
-              {translate(getCopy("prereqs:children"), language)}
-            </li>
-            <li color="black">
-              {translate(getCopy("prereqs:license"), language)}
-            </li>
-            <li color="black">
-              {translate(getCopy("prereqs:bank"), language)}
-            </li>
+            <li color="black">{translate(getCopy("pandemic-eligibility:1"), language)}</li>
+            <li color="black">{translate(getCopy("pandemic-eligibility:2"), language)}</li>
+            <li color="black">{translate(getCopy("pandemic-eligibility:3"), language)}</li>
+            <li color="black">{translate(getCopy("pandemic-eligibility:4"), language)}</li>
+            <li color="black">{translate(getCopy("pandemic-eligibility:5"), language)}</li>
+            <li color="black">{translate(getCopy("pandemic-eligibility:6"), language)}</li>
+            <li color="black">{translate(getCopy("pandemic-eligibility:7"), language)}</li>
+            <li color="black">{translate(getCopy("pandemic-eligibility:8"), language)}</li>
+            <li color="black">{translate(getCopy("pandemic-eligibility:9"), language)}</li>
+            <li color="black">{translate(getCopy("pandemic-eligibility:10"), language)}</li>
+            <li color="black">{translate(getCopy("pandemic-eligibility:11"), language)}</li>
+            <li color="black">{translate(getCopy("pandemic-eligibility:12"), language)}</li>
           </ol>
         </Paragraph>
       </Box>
@@ -71,6 +99,13 @@ const Introduction: React.FC = () => {
             {translate(getCopy("warning:content-2"), language)}
           </Paragraph>
         </Box>
+      </Box>
+      <Box pad="medium">
+        <Paragraph fill={true} color="black">
+          <Text weight={600}>{translate(getCopy("agreement:header"), language)}</Text>
+          <br />
+          <Text weight={400}>{translate(getCopy("agreement:instructions"), language)}</Text>          
+        </Paragraph>
       </Box>
     </>
   );
