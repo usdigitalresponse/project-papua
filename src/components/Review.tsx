246180c81eb54288917ef80a06f9e39cbe216492
Colin "👋" King <me@colinking.co>
2020-04-17 19:15:31 -0700

    Fix review and submit page with some boilerplate

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -1,45 +1,55 @@
-import React, { useContext } from 'react'
-import { Box, Heading, Button } from 'grommet'
+import React, { useContext, useState } from 'react'
+import { Box, Heading, Button, Text } from 'grommet'
 import { getCopy, translate } from '../forms/index'
 import { LanguageContext } from '../contexts/language'
 import Amplify, { API } from 'aws-amplify'
 import awsconfig from '../aws-exports'
+import { FormContext } from '../contexts/form'
 
 Amplify.configure(awsconfig)
 
-function sendData() {
-  const myInit = {
-    body: {
-      metadata: {
-        uuid: Math.floor(Math.random() * 10),
-      },
-      questions: {
-        'first-name': 'Tom',
-        'last-name': 'Nook',
-        occupation: 'Real estate tycoon',
-      },
-    },
-  }
-
-  API.post('resolverAPI', '/claims', myInit)
-    .then((response) => {
-      console.log('rsp:', response)
-    })
-    .catch((error) => {
-      console.log('error: ', error)
-    })
-}
-
 const Review: React.FC<{}> = () => {
   const { language } = useContext(LanguageContext)
+  const { values } = useContext(FormContext)
+
+  const [canSubmit, setCanSubmit] = useState(true)
+
+  const onSubmit = async () => {
+    setCanSubmit(false)
+    try {
+      const resp = await API.post('resolverAPI', '/claims', {
+        body: {
+          metadata: {
+            uuid: Math.floor(Math.random() * 10),
+          },
+          questions: values,
+        },
+      })
+      console.log(resp)
+    } catch (err) {
+      setCanSubmit(true)
+      console.error(err)
+    }
+  }
+
   return (
     <Box pad="medium">
       <Heading margin="none" level={3}>
         {translate(getCopy('submit'), language)}
       </Heading>
-      <Button color="black !important" onClick={sendData}>
-        Post
-      </Button>
+
+      {/* TODO: programmatically render the form values here */}
+
+      <br />
+      <Text>Your claim will not be filed unless you click Submit below.</Text>
+      <br />
+
+      <Button
+        color="black !important"
+        onClick={onSubmit}
+        disabled={!canSubmit}
+        label={translate(getCopy('submit:button'), language)}
+      />
     </Box>
   )
 }
