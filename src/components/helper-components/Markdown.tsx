4b3448fae356a6470c2b16b27606f3a1b824cbb6
Colin "👋" King <me@colinking.co>
2020-04-22 16:00:58 -0700

    remove pgh styles

--- src/components/helper-components/Markdown.tsx
+++ src/components/helper-components/Markdown.tsx
@@ -36,11 +36,6 @@ export const Markdown: React.FC<Props> = ({ margin, size, children }) => {
   // Grommet allows you to override what React component is used for various
   // HTML elements. It handles a few components by default, but there are a few
   // extra cases we want to handle (or customize).
-
-  const paragraphStyles = {
-    // style: { whiteSpace: 'pre-wrap' },
-  }
-
   return (
     <GrommetMarkdown
       components={{
@@ -50,11 +45,11 @@ export const Markdown: React.FC<Props> = ({ margin, size, children }) => {
         },
         p: {
           component: Paragraph,
-          props: merge(paragraphStyles, { fill: true, color: 'black' }, { margin, size }),
+          props: merge({ fill: true, color: 'black' }, { margin, size }),
         },
         span: {
           component: Paragraph,
-          props: merge(paragraphStyles, { fill: true, color: 'black', size: 'small' }, { margin, size }),
+          props: merge({ fill: true, color: 'black', size: 'small' }, { margin, size }),
         },
       }}
     >
