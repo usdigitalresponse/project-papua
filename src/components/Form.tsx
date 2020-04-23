d74568b392b9b47de899254697bcac4b42bd4632
Colin "👋" King <me@colinking.co>
2020-04-23 10:32:04 -0700

    Fix page-level instructions margin

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -15,7 +15,7 @@ const Form: React.FC<Props> = (props) => {
 
   return (
     <Box style={{ padding: '48px' }} direction="column" justify="start">
-      <Heading color="black" margin={{ bottom: 'medium', top: 'none' }} level={3}>
+      <Heading color="black" margin="none" level={3}>
         {translateCopy(page.heading)}
       </Heading>
       {page.instructions && <Markdown>{translateCopy(page.instructions)}</Markdown>}
