a240274cf283022e486c08cafcc86e0f659d4b6c
Colin "👋" King <me@colinking.co>
2020-04-24 17:30:15 -0700

    ISO strings for dates

--- src/components/form-components/DatePicker.tsx
+++ src/components/form-components/DatePicker.tsx
@@ -7,12 +7,13 @@ import { FormContext } from '../../contexts/form'
 const DatePicker: React.FC<{ question: Question }> = (props) => {
   const { question } = props
   const { values, setValue } = useContext(FormContext)
+  const value = values[question.id] as string | undefined
 
   return (
     <ReactDatePicker
       className="date-picker"
-      onChange={(date) => setValue(question, date as Date)}
-      value={values[question.id] as Date}
+      onChange={(date) => setValue(question, (date as Date).toISOString())}
+      value={value ? new Date(value as string) : undefined}
       clearIcon={null}
     />
   )
