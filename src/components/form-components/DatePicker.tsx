3cfd71ffe0d141d8d682b10bec01b2ddd00216dd
Colin "👋" King <me@colinking.co>
2020-04-27 18:28:38 -0700

    fix obo issue in date picker

--- src/components/form-components/DatePicker.tsx
+++ src/components/form-components/DatePicker.tsx
@@ -10,18 +10,33 @@ const DatePicker: React.FC<{ question: Question }> = (props) => {
   const { values, setValue } = useContext(FormContext)
   const value = values[question.id] as string | undefined
 
+  let v: Date | undefined
+  if (value) {
+    const m = moment(value, 'YYYY-MM-DDTHH:mm:ssZ', true)
+    // I wish react-date-picker let you specify the timezone! Instead, it operates
+    // in the local time zone and we just want to send day-truncated dates in RFC3339
+    // format (which requires a timezone, so we specify UTC).
+    //
+    // Since we truncate the timezone to UTC on setValue, we need to replace it when
+    // we provide a value to the date picker. So we add the current user's timezone
+    // offset back to the value to get the original value.
+    const offset = new Date().getTimezoneOffset()
+    v = m.add(offset, 'minutes').toDate()
+  }
+
   return (
     <ReactDatePicker
       className="date-picker"
-      onChange={(date) =>
+      onChange={(date) => {
         setValue(
           question,
-          moment(date as Date)
+          moment
+            .utc(date as Date)
             .startOf('day')
-            .format('YYYY-MM-DDTHH:mm:ss')
+            .format('YYYY-MM-DDTHH:mm:ssZ')
         )
-      }
-      value={value ? new Date(value as string) : undefined}
+      }}
+      value={v}
       clearIcon={null}
     />
   )
--- src/lib/validation.test.ts
+++ src/lib/validation.test.ts
@@ -305,7 +305,7 @@ describe('validation test suite', () => {
       question: {
         type: 'date',
       },
-      value: '2020-01-20T10:00:00',
+      value: '2020-01-20T10:00:00Z',
     },
 
     // Multi select
