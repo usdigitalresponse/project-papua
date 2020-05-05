ab8e23d34e2b1d13479da197619b6de67f66a36f
Colin "👋" King <me@colinking.co>
2020-05-05 16:10:53 -0700

    Replace current fonts with recommended fonts

--- src/components/USDR.tsx
+++ src/components/USDR.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Box } from 'grommet'
 
 interface Props {}
 
@@ -10,5 +11,30 @@ export const USDR: React.FC<Props> = (props) => {
    * - Header with USDR logo, app name, link to GitHub
    * - Footer with "built by etc", links to socials
    */
-  return <>{props.children}</>
+  return (
+    <Box direction="column">
+      <Box>
+        <Header />
+      </Box>
+
+      <Box>{props.children}</Box>
+
+      <Box>
+        <Footer />
+      </Box>
+    </Box>
+  )
+}
+
+const Header: React.FC = () => {
+  return (
+    <Box width="#0050d8" background={{ color: '#0050d8' }} justify="between">
+      <Box>Left</Box>
+      <Box>Right</Box>
+    </Box>
+  )
+}
+
+const Footer: React.FC = () => {
+  return null
 }
