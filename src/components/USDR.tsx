379dd509fe7f92fb577c1f4fdbefbb66f35c9dae
Colin "👋" King <me@colinking.co>
2020-05-08 00:28:04 -0700

    Disable page header for now

--- src/components/USDR.tsx
+++ src/components/USDR.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Heading, Image, Paragraph } from 'grommet'
+import { Box, Image, Paragraph } from 'grommet'
 
 /**
  * The USDR component wraps an underlying component with USDR branding.
@@ -27,25 +27,28 @@ export const USDR: React.FC = (props) => {
 }
 
 const Header: React.FC = () => {
-  return (
-    <Box background={{ color: '#FFFFFF' }} elevation="xsmall" align="center">
-      <Box
-        width="100%"
-        justify="start"
-        style={{ maxWidth: '1200px' }}
-        pad={{ horizontal: 'medium', vertical: '16px' }}
-        direction="row"
-        align="center"
-      >
-        <a href="https://www.usdigitalresponse.org/" target="_blank" rel="noopener noreferrer" tabIndex={-1}>
-          <Image style={{ display: 'block' }} width="32px" src="./USDR-icon-BW.png" margin={{ right: '12px' }} />
-        </a>
-        <Heading level={4} margin="none">
-          Pandemic Unemployment Assistance Demo
-        </Heading>
-      </Box>
-    </Box>
-  )
+  // For now, we will not render a header.
+  return null
+
+  // return (
+  //   <Box background={{ color: '#FFFFFF' }} elevation="xsmall" align="center">
+  //     <Box
+  //       width="100%"
+  //       justify="start"
+  //       style={{ maxWidth: '1200px' }}
+  //       pad={{ horizontal: 'medium', vertical: '16px' }}
+  //       direction="row"
+  //       align="center"
+  //     >
+  //       <a href="https://www.usdigitalresponse.org/" target="_blank" rel="noopener noreferrer" tabIndex={-1}>
+  //         <Image style={{ display: 'block' }} width="32px" src="./USDR-icon-BW.png" margin={{ right: '12px' }} />
+  //       </a>
+  //       <Heading level={4} margin="none">
+  //         Pandemic Unemployment Assistance Demo
+  //       </Heading>
+  //     </Box>
+  //   </Box>
+  // )
 }
 
 const Footer: React.FC = () => {
