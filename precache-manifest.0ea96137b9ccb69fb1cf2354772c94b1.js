034818bd8581da134504bec418a3e020ec74fd16
Alex Gaesser <alexander.gaesser@segment.com>
2020-05-13 10:00:05 -0700

    Updates

---
+++ precache-manifest.0ea96137b9ccb69fb1cf2354772c94b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be9409c7c782ed121a4fc9e239d63183",
+    "url": "https://usdigitalresponse.github.io/project-papua/index.html"
+  },
+  {
+    "revision": "512d57e2003dbdf9cd31",
+    "url": "https://usdigitalresponse.github.io/project-papua/static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "b11ca2604b2f42aae295",
+    "url": "https://usdigitalresponse.github.io/project-papua/static/css/main.b1acdd04.chunk.css"
+  },
+  {
+    "revision": "512d57e2003dbdf9cd31",
+    "url": "https://usdigitalresponse.github.io/project-papua/static/js/2.b6a10297.chunk.js"
+  },
+  {
+    "revision": "77159b0e5580bdc8d2d717b725f6eef5",
+    "url": "https://usdigitalresponse.github.io/project-papua/static/js/2.b6a10297.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b11ca2604b2f42aae295",
+    "url": "https://usdigitalresponse.github.io/project-papua/static/js/main.3f40bf6b.chunk.js"
+  },
+  {
+    "revision": "63ee148baec28b6185bb",
+    "url": "https://usdigitalresponse.github.io/project-papua/static/js/runtime-main.d1a96461.js"
+  }
+]);
